548b3643858a226e0879e6a255d69d6c014db30f
Phillip McLaurin <mclaurinpd@gmail.com>
2021-09-08 14:37:35 -0400

    initial commit

---
+++ src/main/java/engine/Main.java
@@ -0,0 +1,14 @@
+package engine;
+
+import com.github.bhlangonijr.chesslib.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Board b = new Board();
+        System.out.println(b.legalMoves());
+    }
+
+
+
+}
